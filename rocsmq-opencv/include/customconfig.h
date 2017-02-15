e9d6fee890371d4c17798dcef4eaa1b332e28a30
Jens Rapp <rapp.jens@googlemail.com>
2017-02-15 06:48:39 +0100

    some details for opncv config

--- rocsmq-opencv/include/customconfig.h
+++ rocsmq-opencv/include/customconfig.h
@@ -2,16 +2,23 @@
 #define __CUSTOMCONFIG_H
 
 /**
- * pin configuration
+ * color-range
  * 
- * "pinconfig" : [
+ * "color-range" : [
  *   {
- *     "number" : 22,
- *     "direction" : 0,
- *     "mapname" : "motor-1-a"
+ *     "message" : "sensor.vision.red",
+ *     "min-hue" : 0,
+ *     "min-sat" : 64,
+ *     "min-val" : 128,
+ *     "max-hue" : 32,
+ *     "max-sat" : 255,
+ *     "max-val" : 255,
+ * 	   "hotspots" : 3,
  *   },
  *   ...
  *  ]
+ * 
+ *   hotspots - maximum of matches to send
  */
 
 #include <json-c/json.h>
@@ -22,10 +29,33 @@
 #include <rocsmqthread.h>
 #include <configparser.h>
 
-#define CONFIG_KEY_DEVICE		"device"
+#define CONFIG_KEY_DEVICE		(char*)"device"
+#define CONFIG_KEY_ONCOLOR		(char*)"color-range"
+#define CONFIG_KEY_SENDMESSAGE	(char*)"message"
+#define CONFIG_KEY_MIN_HUE		(char*)"min-hue"
+#define CONFIG_KEY_MIN_SAT		(char*)"min-sat"
+#define CONFIG_KEY_MIN_VAL		(char*)"min-val"
+#define CONFIG_KEY_MAX_HUE		(char*)"max-hue"
+#define CONFIG_KEY_MAX_SAT		(char*)"max-sat"
+#define CONFIG_KEY_MAX_VAL		(char*)"max-val"
+#define CONFIG_KEY_HOTSPOTS		(char*)"hotspots" 
+
+#define HUE	0
+#define SAT	1
+#define VAL 2
+
+typedef struct s_colorrange {
+	char message[255];
+	int8_t minvals[3];
+	int8_t maxvals[3];
+	int8_t hotspots;
+} t_colorrange, *p_colorrange;
 
 typedef struct s_clientconfig {
 	char device[255];
+	int num_cr;
+	p_colorrange cr;
+	
 } t_clientconfig, *p_clientconfig;
 
 int custom_config (json_object *json, void * p_datastruct);
--- rocsmq-opencv/src/customconfig.cpp
+++ rocsmq-opencv/src/customconfig.cpp
@@ -1,12 +1,64 @@
 #include "customconfig.h"
 
+int add_color_range(json_object * json, p_clientconfig clientconfig) {
+	int size = json_object_array_length(json);
+	log_message(DEBUG, "having %d items.", size);
+	int i;
+	int load;
+	
+	// allocate memory for color ranges
+	clientconfig->cr = new t_colorrange[size];
+	
+	if (!clientconfig->cr) {
+		log_message(ERROR, "could not allocate memory for color range data");
+		return -1;
+	}
+
+	memset(clientconfig->cr, 0, sizeof(t_colorrange) * size);
+	
+	// read color range data
+	for (i = 0; i < size; i++) {
+		json_object *node = json_object_array_get_idx(json, i);
+		p_colorrange r = & clientconfig->cr[i];
+		
+		get_stringval(node, CONFIG_KEY_SENDMESSAGE, r->message, 255);
+	
+		get_intval(node, CONFIG_KEY_MIN_HUE, & load);
+		r->minvals[HUE] = (int8_t)load;
+		get_intval(node, CONFIG_KEY_MIN_SAT, & load);
+		r->minvals[SAT] = (int8_t)load;
+		get_intval(node, CONFIG_KEY_MIN_VAL, & load);
+		r->minvals[VAL] = (int8_t)load;
+		get_intval(node, CONFIG_KEY_MAX_HUE, & load);
+		r->maxvals[HUE] = (int8_t)load;
+		get_intval(node, CONFIG_KEY_MAX_SAT, & load);
+		r->maxvals[SAT] = (int8_t)load;
+		get_intval(node, CONFIG_KEY_MAX_VAL, & load);
+		r->maxvals[VAL] = (int8_t)load;
+		get_intval(node, CONFIG_KEY_HOTSPOTS, & load);
+		r->hotspots = (int8_t)load;
+		
+	}
+	
+	return 0;
+}
+
 int custom_config (json_object *json, void * p_datastruct) {
+	json_object * color_range = 0;
 	
 	log_message(DEBUG, "Loading custom config.");
 	p_clientconfig config = (p_clientconfig) p_datastruct;
 
 	// get device name
 	get_stringval(json, CONFIG_KEY_DEVICE, config->device, 255);
-	log_message(DEBUG, "Device file %s ", config->device);
-
+	log_message(INFO, "OpenCV device file %s ", config->device);
+	
+	// get color range items
+	get_objval(json, CONFIG_KEY_ONCOLOR, & color_range);
+	if (color_range) {
+		log_message(INFO, "Reading color range elements");
+		if (add_color_range(color_range, config)) {
+			return -1;
+		}
+	}
 } 
